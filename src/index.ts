7e0fe7aee09eda3c6a42fcce4fd0e3fe625fe414
Yen Tran <ytran@marinsoftware.com>
2020-09-15 22:06:20 -0700

    add Company and Google Maps

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hi There!');
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+const company = new Company();
+
+console.log(user);
+console.log(company);
+
+google;
