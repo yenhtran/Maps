8bca6cbc3893a8702c5f834bbc3b37ea617cd381
Yen Tran <ytran@marinsoftware.com>
2020-09-17 22:36:18 -0700

    CustomMap

---
+++ src/CustomMap.ts
@@ -0,0 +1,48 @@
+import { User } from './User';
+import { Company } from './Company';
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  // BAD WAY ======================= Not DRY!
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    });
+  }
+
+  // BETTER WAY ==============================
+  addMarker(mappable: User | Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
+}
