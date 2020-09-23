65a87fb78759c47716d9ece3dfd4050648a53851
Yen Tran <ytran@marinsoftware.com>
2020-09-22 22:12:40 -0700

    Popup Windows

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,13 @@
-import { User } from './User';
-import { Company } from './Company';
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string; // ===> new property
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -14,35 +22,22 @@ export class CustomMap {
     });
   }
 
-  // BAD WAY ======================= Not DRY!
-  addUserMarker(user: User): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
-      },
-    });
-  }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
-      },
-    });
-  }
-
-  // BETTER WAY ==============================
-  addMarker(mappable: User | Company): void {
-    new google.maps.Marker({
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    // Add Listener
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
